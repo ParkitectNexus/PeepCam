a672d8816a996488a04df0336dd5e5aec2d4a8b8
Luuk Holleman <luukholleman@gmail.com>
2016-01-03 14:17:29 +0100

    Compatibility patch with pre-alpha 6

--- PeepCam/Main.cs
+++ PeepCam/Main.cs
@@ -20,5 +20,6 @@ namespace PeepCam
 
         public string Name { get { return "Peep Cam"; } }
         public string Description { get { return "Walk around in your own park!"; } }
+        public string Identifier { get; set; }
     }
 }
